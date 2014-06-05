26aa8ddc3eaa185e4796b0520448f1c5b8960435
Arlandis Lawrence <arlandislawrence@gmail.com>
2014-06-04 21:54:34 -0500

    WIP

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -15,6 +15,45 @@ describe VarnishLogAnalyzer::Parser do
     expect(transaction_zero[0][:transaction_number]).to eq(0)
   end
 
+  shared_examples_for "a transaction retriever" do |transaction, tag, description, details, transaction_number|
+
+    it "includes the tag" do
+      expect(transaction[0][:tag]).to eq(tag)
+    end
+
+    it "includes the description" do
+      expect(transaction[0][:description]).to eq(description)
+    end
+
+    it "includes the details" do
+      expect(transaction[0][:details]).to eq(details)
+    end
+
+    it "includes the transaction number" do
+      expect(transaction[0][:transaction_number]).to eq(transaction_number)
+    end
+
+  end
+
+  describe "#filter_by_transaction" do
+
+    # Taken from sample log file
+    let (:tag) { "CLI" }
+    let (:description) { "Communication betwen varnishd master and child process" }
+    let (:details) { "Rd ping" }
+    let (:transaction_number) { 0 }
+    let (:matching_transactions) { 4 }
+
+    it "returns only transactions matching the transaction number" do
+      transaction_zero = parser.filter_by_transaction(transaction_number)
+      expect(transaction_zero.count).to eq(matching_transactions)
+      expect(transaction_zero[0][:transaction_number]).to eq(transaction_number)
+    end
+
+    it_should_behave_like "a transaction retriever", tag, description, details, transaction_number
+
+  end
+
   it "shows all transactions" do
     transactions = parser.classified_transactions
     expect(transactions.count).to eq(34)
