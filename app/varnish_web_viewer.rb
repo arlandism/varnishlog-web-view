b7c15395d42a1890c665f25abebcd34d32913165
Arlandis Lawrence <arlandislawrence@gmail.com>
2014-06-06 14:06:47 -0500

    Switch to modular style

--- app/varnish_web_viewer.rb
+++ app/varnish_web_viewer.rb
@@ -2,26 +2,32 @@ $LOAD_PATH << "./lib" << "app"
 
 require 'haml'
 require 'multi_json'
-require 'sinatra'
+require 'sinatra/base'
 require 'varnish_log_analyzer/tag_description'
 require 'varnish_log_analyzer/parser'
 
-get '/' do
-  File.open("spec/fixtures/sample_log.txt", "r") do |log_file|
-    @transactions = VarnishLogAnalyzer::Parser.new(log_file.read).classified_transactions
-    haml :index
-  end
-end
+class VarnishWebViewer < Sinatra::Base
 
-get '/:transaction_number' do
-  File.open("spec/fixtures/sample_log.txt") do |log_file|
-      @transaction_number = params[:transaction_number].to_i
-      @transaction_entries = VarnishLogAnalyzer::Parser.new(log_file.read)
-        .filter_by_transaction(@transaction_number)
-      haml :transaction
+  get '/' do
+    File.open("spec/fixtures/sample_log.txt", "r") do |log_file|
+      @transactions = VarnishLogAnalyzer::Parser.new(log_file.read).classified_transactions
+      haml :index
+    end
   end
-end
 
-get '/tag/:tag_name' do
-  MultiJson.encode("#{VarnishLogAnalyzer::TagDescription.for(params[:tag_name])}")
+
+  get '/:transaction_number' do
+    File.open("spec/fixtures/sample_log.txt") do |log_file|
+        @transaction_number = params[:transaction_number].to_i
+        @transaction_entries = VarnishLogAnalyzer::Parser.new(log_file.read)
+          .filter_by_transaction(@transaction_number)
+        haml :transaction
+    end
+  end
+
+  get '/tag/:tag_name' do
+    MultiJson.encode("#{VarnishLogAnalyzer::TagDescription.for(params[:tag_name])}")
+  end
+
+run! if app_file == $0
 end
